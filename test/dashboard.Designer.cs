146a601b93e2d14c1ea24e74ab789fabdeb069a1
SonnearaSola <vipsolobro@gmail.com>
2025-01-08 11:01:29 +0700

    bb

--- test/dashboard.Designer.cs
+++ test/dashboard.Designer.cs
@@ -28,12 +28,23 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(324, 144);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(490, 239);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // dashboard
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button1);
             this.Name = "dashboard";
             this.Size = new System.Drawing.Size(1172, 649);
             this.ResumeLayout(false);
@@ -41,5 +52,7 @@
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
